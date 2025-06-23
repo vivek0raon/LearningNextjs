ed37508c59a3503b6c748eb539379daa9ed20657
vivekoraon4321@gmail.com <contact@spareme.eu.org>
2025-06-24 02:15:42 +0530

    Created createSnippet function , created prismaClient use this client in function, redirect function

--- snippets/src/app/snippet/new/page.tsx
+++ snippets/src/app/snippet/new/page.tsx
@@ -2,18 +2,34 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
+import { prisma } from "@/lib/prisma";
+import { redirect } from "next/navigation";
 import React from "react";
 
 const newSnippet = () => {
+  async function createSnippet(formData: FormData) {
+    "use server"; // user server directive
+    const title = formData.get("title") as string;
+    const code = formData.get("code") as string;
+    const snippet = await prisma.snippet.create({
+      data: {
+        title,
+        code,
+      },
+    });
+    console.log("Created snippet", snippet);
+    redirect("/");
+  }
+
   return (
-    <form className="space-y-3">
+    <form action={createSnippet} className="space-y-3">
       <div className="space-y-3">
         <Label>Title</Label>
         <Input type="text" name="title" id="title" />
       </div>
       <div className="space-y-3">
         <Label>Code</Label>
-        <Textarea name="title" id="title" />
+        <Textarea name="code" id="code" />
       </div>
       <Button type="submit">New</Button>
     </form>
