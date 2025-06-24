c7c9c29dd5eb00124a5b3ea7e222034663e00c14
vivekoraon4321@gmail.com <contact@spareme.eu.org>
2025-06-24 23:50:41 +0530

    Setup the nextjs project, installed other packages like shadcn , prisma, installed shadcn compoenets, setup prisma to use sqlite then created model in schema.prisma

---
+++ discuss/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>HomePage</h1>
+      <Button>Lets Build discuss app</Button>
+    </div>
+  );
+}
