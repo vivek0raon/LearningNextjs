dcd4481f3a8c623c1020cee8d2783355e449d208
vivekoraon4321@gmail.com <contact@spareme.eu.org>
2025-06-24 18:30:25 +0530

    Moved createSnippet server action to actions folder, fixed not string type error, Used userActionState hook, show the message in page

--- snippets/src/actions/index.tsx
+++ snippets/src/actions/index.tsx
@@ -22,3 +22,25 @@ export const deleteSnippet = async (id: number) => {
   });
   redirect("/");
 };
+
+export async function createSnippet(
+  prevState: { message: string },
+  formData: FormData
+) {
+  const title = formData.get("title");
+  const code = formData.get("code");
+  if (typeof title != "string" || title.length < 4) {
+    return { message: "Title is required and must be longer" };
+  }
+  if (typeof code != "string" || code.length < 4) {
+    return { message: "Code is required" };
+  }
+  const snippet = await prisma.snippet.create({
+    data: {
+      title,
+      code,
+    },
+  });
+  console.log("Created snippet", snippet);
+  redirect("/");
+}
--- snippets/src/app/snippet/new/page.tsx
+++ snippets/src/app/snippet/new/page.tsx
@@ -1,28 +1,17 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
-import { prisma } from "@/lib/prisma";
-import { redirect } from "next/navigation";
-import React from "react";
-
-const newSnippet = () => {
-  async function createSnippet(formData: FormData) {
-    "use server"; // user server directive
-    const title = formData.get("title") as string; 
-    const code = formData.get("code") as string;
-    const snippet = await prisma.snippet.create({
-      data: {
-        title,
-        code,
-      },
-    });
-    console.log("Created snippet", snippet);
-    redirect("/");
-  }
+import React, { useActionState } from "react";
+import { createSnippet } from "@/actions";
+const NewSnippet = () => {
+  const [formStateData, callServerAction] = useActionState(createSnippet, {
+    message: "",
+  });
 
   return (
-    <form action={createSnippet} className="space-y-3">
+    <form action={callServerAction} className="space-y-3">
       <div className="space-y-3">
         <Label>Title</Label>
         <Input type="text" name="title" id="title" />
@@ -31,9 +20,14 @@ const newSnippet = () => {
         <Label>Code</Label>
         <Textarea name="code" id="code" />
       </div>
+      {formStateData.message && (
+        <div className="p-2 bg-red-300 border-2 border-red-600">
+          {formStateData.message}
+        </div>
+      )}
       <Button type="submit">New</Button>
     </form>
   );
 };
 
-export default newSnippet;
+export default NewSnippet;
