4d9a24777533beb471439b61d476c3b1abe216a7
vivekoraon4321@gmail.com <contact@spareme.eu.org>
2025-06-24 20:13:36 +0530

    Error handling in nextjs

--- snippets/src/actions/index.tsx
+++ snippets/src/actions/index.tsx
@@ -27,20 +27,26 @@ export async function createSnippet(
   prevState: { message: string },
   formData: FormData
 ) {
-  const title = formData.get("title");
-  const code = formData.get("code");
-  if (typeof title != "string" || title.length < 4) {
-    return { message: "Title is required and must be longer" };
+  try {
+    const title = formData.get("title");
+    const code = formData.get("code");
+    if (typeof title != "string" || title.length < 4) {
+      return { message: "Title is required and must be longer" };
+    }
+    if (typeof code != "string" || code.length < 4) {
+      return { message: "Code is required" };
+    }
+    await prisma.snippet.create({
+      data: {
+        title,
+        code,
+      },
+    });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      return { message: error.message };
+    }
+    return { message: "An unknown error occurred" };
   }
-  if (typeof code != "string" || code.length < 4) {
-    return { message: "Code is required" };
-  }
-  const snippet = await prisma.snippet.create({
-    data: {
-      title,
-      code,
-    },
-  });
-  console.log("Created snippet", snippet);
   redirect("/");
 }
