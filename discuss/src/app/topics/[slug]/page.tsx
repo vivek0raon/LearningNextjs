176c97da4217d2896aa6b0142d2650bf660f4087
vivekoraon4321@gmail.com <contact@spareme.eu.org>
2025-06-25 22:48:47 +0530

    Create new routes for post creation

---
+++ discuss/src/app/topics/[slug]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TopicShowPage = () => {
+  return (
+    <div>TopicShowPage</div>
+  )
+}
+
+export default TopicShowPage
---
+++ discuss/src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PostShowPage = () => {
+  return (
+    <div>PostShowPage</div>
+  )
+}
+
+export default PostShowPage
