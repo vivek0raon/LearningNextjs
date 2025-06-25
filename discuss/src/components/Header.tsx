9fca63b893e41ee81cce8a397245707fa8fa31a7
vivekoraon4321@gmail.com <contact@spareme.eu.org>
2025-06-25 21:30:28 +0530

    Added popover and separator from shadcn, In header added avatar and popover signout

--- discuss/src/components/Header.tsx
+++ discuss/src/components/Header.tsx
@@ -2,8 +2,15 @@ import React from "react";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
 import { signIn } from "@/app/actions/sign-in";
+import { signOut } from "@/app/actions/sign-out";
+import { auth } from "@/auth";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
+import { LogOut } from "lucide-react";
+import { Separator } from "./ui/separator";
 
-const Header = () => {
+const Header = async () => {
+  const session = await auth();
   return (
     <div className="grid grid-cols-3 h-14 items-center">
       <div className="flex justify-start">
@@ -13,12 +20,34 @@ const Header = () => {
         <Input type="text" placeholder="Search post..." />
       </div>
       <div className="flex justify-end gap-2">
-        <form action={signIn}>
-          <Button variant={"outline"}>Sign In</Button>
-        </form>
-        <form action={signIn}>
-          <Button>Sign up</Button>
-        </form>
+        {session?.user ? (
+          <Popover>
+            <PopoverTrigger asChild>
+              <Avatar>
+                <AvatarImage src={session.user.image || ""} alt="@shadcn" />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+            </PopoverTrigger>
+            <PopoverContent>
+              <h1>{session.user.name}</h1>
+              <Separator className="my-2" />
+              <form action={signOut}>
+                <Button type="submit">
+                  <LogOut /> Sign Out
+                </Button>
+              </form>
+            </PopoverContent>
+          </Popover>
+        ) : (
+          <>
+            <form action={signIn}>
+              <Button variant={"outline"}>Sign In</Button>
+            </form>
+            <form action={signIn}>
+              <Button>Sign up</Button>
+            </form>
+          </>
+        )}
       </div>
     </div>
   );
