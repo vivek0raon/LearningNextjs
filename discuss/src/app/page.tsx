1755d93858784795c1d7ecdd96cbcbebbb032202
vivekoraon4321@gmail.com <contact@spareme.eu.org>
2025-06-26 19:34:50 +0530

    created fetchTopPost query run prisma query to to findmany post order comments's count in desc order and included other field from relation table, send the fetchTopPost as a prop to PostList

--- discuss/src/app/page.tsx
+++ discuss/src/app/page.tsx
@@ -1,13 +1,16 @@
+import PostList from "@/components/posts/PostList";
 import TopicCreateForm from "@/components/topics/TopicCreateForm";
+import { fetchTopPost } from "@/lib/query/post";
 
 export default async function Home() {
   return (
     <div className="grid grid-cols-4 gap-4 p-4">
       <div className="col-span-3">
         <h1 className="text-xl fond-bold m-2">HomePage</h1>
+        <PostList fetchData={fetchTopPost} />
       </div>
       <div>
-        <TopicCreateForm/>
+        <TopicCreateForm />
       </div>
     </div>
   );
--- discuss/src/lib/query/post.ts
+++ discuss/src/lib/query/post.ts
@@ -39,3 +39,19 @@ export const fetchPostByTopicSlug = async (
     },
   });
 };
+
+export const fetchTopPost = async (): Promise<PostWithData[]> => {
+  return prisma.post.findMany({
+    orderBy: {
+      comments: {
+        _count: "desc",
+      },
+    },
+    include: {
+      topic: { select: { slug: true } },
+      _count: { select: { comments: true } },
+      user: { select: { name: true } },
+    },
+    take: 5,
+  });
+};
